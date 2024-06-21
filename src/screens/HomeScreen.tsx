8c3b1e828ecb50e86530a94265696dccf79b8866
SebastianHinestroza12 <menas7527@gmail.com>
2024-06-20 22:39:18 -0500

    feat:Added the bottom tab and the content of its components

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 export const HomeScreen = () => {
   return (
-    <View style={ styles.homeBackground}>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  homeBackground: {
-    flex: 1,
-    backgroundColor: '#FFF',
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
--- src/screens/TabBottomScreen.tsx
+++ src/screens/TabBottomScreen.tsx
@@ -1,12 +1,23 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { HomeScreen } from './HomeScreen';
-import { BookingScreen} from './BookingScreen';
-import {ProfileScreen } from './ProfileScreen';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {HomeScreen} from './HomeScreen';
+import {BookingScreen} from './BookingScreen';
+import {ProfileScreen} from './ProfileScreen';
+import {Button} from 'react-native';
+import {useAppDispatch} from '../Redux/Store/hook';
+import {setAuthenticated} from '../Redux/ReducerConfig/Reducers/Aunthenticated';
 
 const Tab = createBottomTabNavigator();
 
-export const TabBottomScreen = () => {
+export const TabBottomScreen = ({navigation}: any) => {
+  const dispatch = useAppDispatch();
+
+  const handleLogout = () => {
+    dispatch(setAuthenticated(null));
+    navigation.navigate('LoginScreen');
+  };
+
   return (
     <Tab.Navigator
       screenOptions={{
@@ -16,10 +27,54 @@ export const TabBottomScreen = () => {
           backgroundColor: '#101010',
         },
       }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Booking" component={BookingScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen
+        options={{
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#fff',
+            shadowColor: '#000',
+            shadowOffset: {width: 0, height: 2},
+            shadowOpacity: 0.1,
+            shadowRadius: 3.84,
+            elevation: 5,
+          },
+        }}
+        name="Map"
+        component={HomeScreen}
+      />
+      <Tab.Screen
+        options={{
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#fff',
+            shadowColor: '#000',
+            shadowOffset: {width: 0, height: 2},
+            shadowOpacity: 0.1,
+            shadowRadius: 3.84,
+            elevation: 5,
+          },
+        }}
+        name="Booking"
+        component={BookingScreen}
+      />
+      <Tab.Screen
+        options={{
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#fff',
+            shadowColor: '#000',
+            shadowOffset: {width: 0, height: 2},
+            shadowOpacity: 0.1,
+            shadowRadius: 3.84,
+            elevation: 5,
+          },
+          headerRight: () => (
+            <Button onPress={handleLogout} title="Logout" color="#E38800" />
+          ),
+        }}
+        name="Profile"
+        component={ProfileScreen}
+      />
     </Tab.Navigator>
   );
 };
-
